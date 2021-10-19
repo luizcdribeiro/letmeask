77efc8026e2dc953841bd5b3a55795b5d0f24004
Luiz Carlos Ribeiro <luizjunior.dantas@gmail.com>
2021-10-19 00:43:52 -0300

    aplicação letmeask utilizando firebase

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,38 @@
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+import '../styles/auth.scss';
+import { Button } from '../components/Button';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../hooks/useAuth';
+
+export function NewRoom() {
+
+  const { user } = useAuth()
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={ illustrationImg } alt="Ilustração simbolizando perguntas e repostas" />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
+      </aside>
+
+      <main>
+        <div className="main-content">
+          <img src={ logoImg } alt="Letmeask" />
+          <h2>Criar uma nova sala</h2>
+          <form>
+            <input 
+              type="text"
+              placeholder="Nome da Sala"
+             />
+             <Button type="submit">
+               Criar sala
+             </Button>
+          </form>
+          <p>Quer entrar em uma sala já existente? <Link to="/">clique aqui</Link></p>
+        </div>
+      </main>
+    </div>
+  )
+}
